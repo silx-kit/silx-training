a55c502695842a8e394a242ac2fcfbecb9ffb8b8
Pierre <pierre.knobel@esrf.fr>
2018-03-19 14:41:53 +0100

    fix fwhm estimation (in x coords, not indices) and improve prints

--- silx/processing/fit/fitting_multi_scans.py
+++ silx/processing/fit/fitting_multi_scans.py
@@ -10,28 +10,33 @@ with silx.io.open("data/31oct98.dat") as specfile:
     for scan in ["21.1", "22.1", "23.1", 
                  "24.1", "25.1", "26.1",
                  "27.1"]:
+        print("Processing scan %s" % scan)
         measurement_group = specfile["/" + scan + "/measurement"]
-        xdata = measurement_group["TZ3"]
-        ydata = measurement_group["If2"]
+        xdata = measurement_group["TZ3"][()]
+        ydata = measurement_group["If2"][()]
 
-        guessed_fwhm = guess_fwhm(ydata)
-        peaks_indices = peak_search(ydata, guessed_fwhm)
+        guessed_fwhm = guess_fwhm(ydata) * ((xdata[-1] - xdata[0]) / len(xdata))
+        peaks_indices = peak_search(ydata, guessed_fwhm, sensitivity=10)
+
+        print("\tFound %d peaks" % len(peaks_indices))
 
         positions = [xdata[int(i)] for i in peaks_indices]
         heights = [ydata[int(i)] for i in peaks_indices]
          
         estimated_params = []
+        constraints = []
         for h, p in zip(heights, positions):
             estimated_params += [h, p, guessed_fwhm]
+            constraints += [[], [], []]
         
         try:
             fitted_params, cov_matrix = leastsq(model=sum_gauss, 
                                                 xdata=xdata, ydata=ydata, 
                                                 p0=estimated_params)
         except LinAlgError:
-            print("Unable to fit scan %s" % scan)
+            print("\tUnable to fit the data")
         else:
-            print("Fit results for scan %s:" % scan)
+            print("\tFit results:")
             for i in range(len(fitted_params) // 3):
                 h, p, fwhm = fitted_params[3*i:3*i+3]
-                print("\tPeak %d: heigth %f, position %f, fwhm %f" % (i, h, p, fwhm))
+                print("\t\tPeak %d: heigth %f, position %f, fwhm %f" % (i, h, p, fwhm))
