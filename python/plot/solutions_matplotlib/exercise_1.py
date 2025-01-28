39a07f5e6ab6c4ba1991761e20a18e3935f5eac0
payno <henri.payno@esrf.fr>
2025-01-28 08:53:10 +0100

    python-training: add a simple matplotlib training.

--- python/plot/solutions_matplotlib/exercise_1.py
+++ python/plot/solutions_matplotlib/exercise_1.py
@@ -1,21 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Step 1: Generate 100 points between 0 and 2*pi
-x = np.linspace(0, 2 * np.pi, 100)
+# Step 1: Load spectrum
+array = np.loadtxt("data/spectrum.txt")
 
-# Step 2: Calculate sine and cosine values
-y_sin = np.sin(x)
-y_cos = np.cos(x)
+# Step 2: Extract energy and mu
+energy = array[:, 0]
+mu = array[:, 1]
 
 # Step 3: Plot both sine and cosine curves
-plt.plot(x, y_sin, label='Sine Curve', color='b')  # Blue for sine curve
-plt.plot(x, y_cos, label='Cosine Curve', color='r')  # Red for cosine curve
+plt.plot(energy, mu, label='mu Curve', color='blue')
 
 # Step 4: Add labels, title, and legend
-plt.xlabel('X (radians)')
-plt.ylabel('Y (value)')
-plt.title('Sine and Cosine Curves')
+plt.xlabel('energy (keV)')
+plt.ylabel('absorption (a.u.)')
 plt.legend()
 
 # Show the plot
---
+++ python/plot/solutions_matplotlib/exercise_2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+array = np.loadtxt("data/spectrum.txt")
+energy = array[:, 0]
+mu = array[:, 1]
+normalized_energy = array[:, 2]
+pre_edge = array[:, 3]
+post_edge = array[:, 4]
+
+ax1, ax2 = plt.subplots(1, 2, figsize=(20, 10))[-1]
+
+ax1.plot(normalized_energy, mu)
+ax1.plot(normalized_energy, pre_edge, color="orange", linestyle=':')
+ax1.fill_between(normalized_energy, 0, pre_edge, color="orange", alpha=.5)
+
+ax1.plot(normalized_energy, post_edge, color="green", linestyle=':')
+ax1.fill_between(normalized_energy, 4, post_edge, color="green", alpha=.5)
+
+ax1.set_xlabel("Energy (eV)")
+ax1.set_ylabel("absorption")
+
+ax2.plot(normalized_energy, mu, label="mu")
+ax2.set_xlabel("Energy (eV)")
+ax2.set_ylabel("absorption")
+
+E0 = 8981.1
+
+ax2.axvline(E0, color="red", linestyle='--', linewidth=3)
+ax2.text(8900, 1.5, 'E0', color="red")
+
+
+# Show the plot
+plt.show()
